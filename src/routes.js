d5936e06f1252a7edaf7e0720ba0a53c6efaaba1
jose antonio <rodrigues.jantonio84@gmail.com>
2020-11-20 17:34:58 -0300

    Comite de teste

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const UserControllers = require('./controllers/UserController');
 
 
 const routes = express.Router();
 
-routes.get('/',(req,res)=>{
-    return res.json({hello: 'world'});
-})
+routes.post('/users',UserControllers.Store)
 
 module.exports = routes;
+
+// testar chegada de rota
+//routes.post('/users',(req,res)=>{
+  //  return res.json({hello: 'world'});
+//})
